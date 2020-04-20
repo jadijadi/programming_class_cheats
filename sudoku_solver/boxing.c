5cb7bc58461ab98577018c90f8ffd075db1f1c67
Farzin <monseffarzin@gmail.com>
2020-04-20 23:55:31 +0430

    new color added

--- sudoku_solver/boxing.c
+++ sudoku_solver/boxing.c
@@ -1,6 +1,7 @@
 #include "boxing.h"
 
 void box_up(WINDOW* win){
+    wattron(win, COLOR_PAIR(3) | A_BOLD);
     waddch(win, ACS_ULCORNER);
     for (int k = 0; k < 8; ++k){
         waddch(win, ACS_HLINE);
@@ -12,9 +13,11 @@ void box_up(WINDOW* win){
     waddch(win, ACS_HLINE);
     waddch(win, ACS_HLINE);
     waddch(win, ACS_URCORNER);
+    wattroff(win, COLOR_PAIR(3) | A_BOLD);
 }
 
 void box_down(WINDOW* win){
+    wattron(win, COLOR_PAIR(3) | A_BOLD);
     waddch(win, ACS_LLCORNER);
     for (int k = 0; k < 8; ++k){
         waddch(win, ACS_HLINE);
@@ -26,22 +29,22 @@ void box_down(WINDOW* win){
     waddch(win, ACS_HLINE);
     waddch(win, ACS_HLINE);
     waddch(win, ACS_LRCORNER);
+    wattroff(win, COLOR_PAIR(3) | A_BOLD);
 }
 
 void box_line(WINDOW* win){
-    waddch(win, ACS_LTEE);
+    waddch(win, COLOR_PAIR(3) | ACS_LTEE | A_BOLD);
     for (int k = 0; k < 8; ++k){
         waddch(win, ACS_HLINE);
         waddch(win, ACS_HLINE);
         waddch(win, ACS_HLINE);
-        waddch(win, ACS_PLUS);
+        if ((k+1) % 3 == 0)
+            waddch(win, COLOR_PAIR(3) | ACS_PLUS | A_BOLD);
+        else
+            waddch(win, ACS_PLUS);
     }
     waddch(win, ACS_HLINE);
     waddch(win, ACS_HLINE);
     waddch(win, ACS_HLINE);
-    waddch(win, ACS_RTEE);
+    waddch(win, COLOR_PAIR(3) | ACS_RTEE | A_BOLD);
 }
-
-
-
-    
