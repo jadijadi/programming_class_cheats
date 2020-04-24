2fc2511133a30e2bdf6ee9a46e972d5a35d0645b
sadrasabouri <sabouri.sadra@gmail.com>
2020-04-24 15:23:12 +0430

    add : changing type of files added.

--- gif_hider/gif_hider.c
+++ gif_hider/gif_hider.c
@@ -4,6 +4,7 @@
 
 #define MAX_FILE_SIZE (1024*1024)
 #define MAX_FILE_NAME (100)
+#define MAX_FILE_EXT (10)
 
 int main (int argc, char *argv[])
 {
@@ -13,8 +14,8 @@ int main (int argc, char *argv[])
 		printf("Hide your .gif files.\n");
 		printf("Example: gif_hider -e 70 phpconf.gif\n\n");
 		printf("OPTIONS:\n");
-		printf("  -e    Encryption\n");
-		printf("  -d    Decryption\n");
+		printf("  -e    Encryption - for .gif files.\n");
+		printf("  -d    Decryption - for .cif(Coded gif) files.\n");
 		printf("CODE the code that file is going to be coded with it.\n");
 		printf("FILE the .gif/.cif file name.\n");
 		return 0;
@@ -27,7 +28,19 @@ int main (int argc, char *argv[])
 
 	char fname[MAX_FILE_NAME];
 	strcpy(fname, argv[3]);
-	
+	int l = 0;
+	while (fname[l++] != '.' && l < MAX_FILE_NAME);
+	char fext[MAX_FILE_EXT];
+	char name[MAX_FILE_NAME];
+	strcpy(fext, fname + l - 1);
+	strncpy(name, fname, l - 1);
+
+	if ( (!strcmp(opt, "-e") && strcmp(fext, ".gif")) || (!strcmp(opt, "-d") && strcmp(fext, ".cif")))
+	{
+		printf("Wrong Input!\n");
+		printf("Run gif_hider -h for help.\n");
+		return 0;
+	}
 	FILE *fptr;
 	char data[MAX_FILE_SIZE];
 
@@ -38,7 +51,10 @@ int main (int argc, char *argv[])
 		data[i++] = fgetc(fptr);
 	fclose(fptr);
 
-	fptr = fopen(fname, "wb");
+	if (!strcmp(opt, "-e"))
+		fptr = fopen(strcat(name, ".cif"), "wb");
+	else
+		fptr = fopen(strcat(name, ".gif"), "wb");
 
 	for (int j=0; j<i-1; j++)
 		fputc((data[j] ^ code), fptr);
