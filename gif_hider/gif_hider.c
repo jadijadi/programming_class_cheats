7776101579645e2fe8e8c33de7d11a8bd394eae6
sadrasabouri <sabouri.sadra@gmail.com>
2020-04-24 13:26:01 +0430

    add : filename input from args added.

--- gif_hider/gif_hider.c
+++ gif_hider/gif_hider.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_FILE_SIZE (1024*1024)
-int main ()
+#define MAX_FILE_NAME (100)
+
+int main (int argc, char *argv[])
 {
+	char fname[MAX_FILE_NAME];
+	strcpy(fname, argv[1]);
+	
 	FILE *fptr;
 	char data[MAX_FILE_SIZE];
 
-	fptr = fopen("phpconf.gif", "rb");
+	fptr = fopen(fname, "rb");
 
 	int i = 0;
 	while (!feof(fptr))
 		data[i++] = fgetc(fptr);
 	fclose(fptr);
 
-	fptr = fopen("phpconf.gif", "wb");
+	fptr = fopen(fname, "wb");
 
 	for (int j=0; j<i-1; j++)
 		fputc((data[j] ^ 0x60), fptr);
@@ -21,8 +27,3 @@ int main ()
 
 	return 0;
 }
-
-
-
-
-
