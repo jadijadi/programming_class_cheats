f357fd0e45588e289569c571f4112bb61d1e7311
nematzade <mohammadnematzade@yahoo.com>
2020-04-24 12:01:51 +0430

    resolving issue_fun,for fun

--- gif_hider/gif_hider.c
+++ gif_hider/gif_hider.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 #define MAX_FILE_SIZE (1024*1024)
-int main ()
+int main (int argc, char *argv[])
 {
 	FILE *fptr;
 	char data[MAX_FILE_SIZE];
 
-	fptr = fopen("phpconf.gif", "rb");
+	// fn => file name.
+	char *fn = argv[1];
+
+	fptr = fopen(fn, "rb");
 
 	int i = 0;
 	while (!feof(fptr))
 		data[i++] = fgetc(fptr);
 	fclose(fptr);
 
-	fptr = fopen("phpconf.gif", "wb");
+	fptr = fopen(fn, "wb");
 
 	for (int j=0; j<i-1; j++)
 		fputc((data[j] ^ 0x60), fptr);
