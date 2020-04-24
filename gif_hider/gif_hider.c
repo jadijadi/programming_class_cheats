6c219352696dc504c49c8a66a8aab19b0c5ccf22
sadrasabouri <sabouri.sadra@gmail.com>
2020-04-24 13:32:23 +0430

    add : code input from args added.

--- gif_hider/gif_hider.c
+++ gif_hider/gif_hider.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_FILE_SIZE (1024*1024)
 #define MAX_FILE_NAME (100)
 
 int main (int argc, char *argv[])
 {
+	char code = 0;
+	code = atoi(argv[1]);
+
 	char fname[MAX_FILE_NAME];
-	strcpy(fname, argv[1]);
+	strcpy(fname, argv[2]);
 	
 	FILE *fptr;
 	char data[MAX_FILE_SIZE];
@@ -22,7 +26,7 @@ int main (int argc, char *argv[])
 	fptr = fopen(fname, "wb");
 
 	for (int j=0; j<i-1; j++)
-		fputc((data[j] ^ 0x60), fptr);
+		fputc((data[j] ^ code), fptr);
 	fclose(fptr);
 
 	return 0;
