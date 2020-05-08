8455c47e4b981f62f81e977f282387080e47ceef
Farzin <monseffarzin@gmail.com>
2020-05-08 21:07:27 +0430

    switches and arguments added to program

--- gif_hider/gif_hider.c
+++ gif_hider/gif_hider.c
@@ -1,14 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
-int main ()
+char *program_name;
+
+void print_usage(FILE*, int);
+void encrypt(const char *, const char *, long int);
+
+int main (int argc, char *argv[])
 {
+	program_name = argv[0];
+
+	int next_option;
+	long int code = 1;
+	const char *short_options = "hi:o:x:";
+
+	const struct option long_options[] = {
+		{ "help",   no_argument,	0,   'h'},
+		{ "input",  required_argument,	0,   'i'},
+		{ "output", required_argument,  0,   'o'},
+		{ "hex",    required_argument,  0,   'x'},
+		{ NULL,        0,		NULL, 0 }
+	};
+
+	const char *input_file = NULL;
+	const char *output_file = NULL;
+
+	do {
+		next_option = getopt_long(argc, argv,
+				short_options, long_options, NULL);
+		switch(next_option){
+			case 'h':
+				print_usage(stdout, 0);
+				break;
+			case 'i':
+				input_file = optarg;
+				break;
+			case 'o':
+				output_file = optarg;
+				break;
+			case 'x':
+				code = (int)strtol(optarg, NULL, 16);
+				break;
+			case '?':
+				print_usage(stderr, 1);
+				break;
+		}
+	} while (next_option != -1);
+
+	if (input_file && output_file){
+		encrypt(input_file, output_file, code);
+	}
+	
+	printf("done whit key: %p\n", code);
+
+	return 0;
+}
+void print_usage(FILE *stream, int exit_code)
+{
+	fprintf(stream,
+			"\n"
+			"Usage: %s [OPTION] [ARGUMENTS]\n"
+			"-h  --help		print this menue\n"
+			"-i  --inputfile	input file  (to read from)\n"
+			"-o  --outputfile	output file (to write to)\n"
+			"-x  --hex		hexadecimal key for encryption\n\n",
+			program_name);
+
+	exit(exit_code);
+}
+
+void encrypt(const char *input_file, const char *output_file, long int code)
+{
+
 	FILE *fptro;
 	FILE *fptrt;
 	char data;
 
-	fptro = fopen("phpconf.gif", "rb");
-	fptrt = fopen(".phpconf.gif", "wb");
+	fptro = fopen(input_file, "rb");
+	fptrt = fopen(".tmp.gif", "wb");
 
 	if (!fptro || !fptrt){
 		fprintf(stderr, "Error to opening files\n");
@@ -18,14 +88,13 @@ int main ()
 	int i = 0;
 	while (!feof(fptro)){
 		data = fgetc(fptro);
-		fputc((data ^ 0x60), fptrt);
+		fputc((data ^ code), fptrt);
 	}
 
 	fclose(fptro);
 	fclose(fptrt);
 	
-	remove("phpconf.gif");
-	rename(".phpconf.gif", "phpconf.gif");
+	remove(input_file);
+	rename(".tmp.gif", output_file);
 
-	return 0;
 }
