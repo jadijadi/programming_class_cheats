7a4c39d061836de35074ed189d3177266dc937b3
yilmaz <72473367+yilmaz7@users.noreply.github.com>
2020-11-09 01:17:15 +0330

    Update todays_rotor_generator.py

--- enigma/todays_rotor_generator.py
+++ enigma/todays_rotor_generator.py
@@ -1,21 +1,36 @@
 import random
 import pickle
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz '
+#Adding (),:;'' to current program to make it more user-friendly
+alphabet = "abcdefghijklmnopqrstuvwxyz ''():;,"
 
-r1 = list(alphabet)
-random.shuffle(r1)
-r1 = ''.join(r1)
+def main():
+  r1 = list(alphabet)
+  random.shuffle(r1)
+  r1 = ''.join(r1)
 
+  r2 = list(alphabet)
+  random.shuffle(r2)
+  r2 = ''.join(r2)
 
-r2 = list(alphabet)
-random.shuffle(r2)
-r2 = ''.join(r2)
+  r3 = list(alphabet)
+  random.shuffle(r3)
+  r3 = ''.join(r3)
 
-r3 = list(alphabet)
-random.shuffle(r3)
-r3 = ''.join(r3)
+  f = open('./todays_rotor_state.enigma', 'wb')
+  pickle.dump((r1, r2, r3), f)
+  f.close()
 
-f = open('./todays_rotor_state.enigma', 'wb')
-pickle.dump((r1, r2, r3), f)
-f.close()
+  #Saving keys in form of .txt file in case we need them!!
+  f1 = open("r1(keys).txt", "a")
+  f1.write(R1)
+  f1.close()
+
+  f2 = open("r2(keys).txt", "a")
+  f2.write(R2)
+  f2.close()
+
+  f3 = open("r3(keys).txt", "a")
+  f3.write(r3)
+  f3.close()
+if __name__=="__main__":main()
