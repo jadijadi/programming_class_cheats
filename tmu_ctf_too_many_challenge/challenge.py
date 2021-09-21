8eecb95855f565c3f7dc0dda9223e2af9fda7cca
milad <zmilad97@gmail.com>
2021-09-21 19:25:44 +0430

    fixed for jadi

--- tmu_ctf_too_many_challenge/challenge.py
+++ tmu_ctf_too_many_challenge/challenge.py
@@ -10,17 +10,15 @@ def func(x):
     print ('func started for %s' % x)
     # Returns the number of distinct pairs (y, z) from the numbers in the file "numbers.txt" whose y != z and (y + z) == x
     # Note that two pairs (y, z) and (z, y) are considered the same and are counted only once
-    ans = set() 
+    ans=set()
+    nums = {} 
     step = 0
     for i in numbers:
+        nums[step] = i
+        step = step+1
+    for i in numbers:
         j = x - i # we are looking for j where j+i == x
-        if j in numbers:
-            if j == i:
-                continue
-            elif j > i:
-                ans.add((j,i))
-            else:
-                ans.add((i,j))
+        ans.add(nums.get(j))
     return len(ans) 
 
 
