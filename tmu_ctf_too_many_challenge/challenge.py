f5beb1aa527981de609443ec69327882158b7ceb
Alireza <Alirezasampoor@gmail.com>
2021-09-22 00:01:28 +0330

    update algorithm

--- tmu_ctf_too_many_challenge/challenge.py
+++ tmu_ctf_too_many_challenge/challenge.py
@@ -1,27 +1,17 @@
-numbers = []
+numbers = set()
 print ('starting')
 with open("numbers.txt") as f:
         content = f.readlines()
 for n in content:
-    numbers.append(int(n.strip()))
+    numbers.add(int(n.strip()))
 print ('numbers are ready')
 
 def func(x):
     print ('func started for %s' % x)
     # Returns the number of distinct pairs (y, z) from the numbers in the file "numbers.txt" whose y != z and (y + z) == x
     # Note that two pairs (y, z) and (z, y) are considered the same and are counted only once
-    ans = set() 
-    step = 0
-    for i in numbers:
-        j = x - i # we are looking for j where j+i == x
-        if j in numbers:
-            if j == i:
-                continue
-            elif j > i:
-                ans.add((j,i))
-            else:
-                ans.add((i,j))
-    return len(ans) 
+    ans = {x-y for y in numbers}
+    return len(ans.intersection(numbers))//2 
 
 
 def get_flag(res):
