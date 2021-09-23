58b72b489c921ed4a79014d5d971f24a0f22473a
Mahdi Sedaghat <41582368+Mahdi171@users.noreply.github.com>
2021-09-23 17:48:27 +0200

    Using Trie structure

--- tmu_ctf_too_many_challenge/Trie.py
+++ tmu_ctf_too_many_challenge/Trie.py
@@ -11,65 +11,44 @@ class TrieNode(object):
 
 def add(root, int: int):
     """
-    Adding an int in the trie structure
+    Adding an int in the Trie structure
     """
     node = root
     for char in int:
         found_in_child = False
-        # Search for the character in the children of the present `node`
         for child in node.children:
             if child.char == char:
-                # We found it, increase the counter by 1 to keep track that another
-                # int has it as well
                 child.counter += 1
-                # And point the node to the child that contains this char
                 node = child
                 found_in_child = True
                 break
-        # We did not find it so add a new chlid
         if not found_in_child:
             new_node = TrieNode(char)
             node.children.append(new_node)
-            # And then point node to the new child
             node = new_node
-    # Everything finished. Mark it as the end of a int.
     node.int_finished = True
 
 
 def find_prefix(root, prefix: int) -> int:
-    """
-    Check and return 
-      1. If the prefix exsists in any of the ints we added so far
-      2. If yes then how may ints actually have the prefix
-    """
     node = root
-    # If the root node has no children, then return False.
-    # Because it means we are trying to search in an empty trie
     if not root.children:
         return 0
     for char in str(prefix):
         char_not_found = True
-        # Search through all the children of the present `node`
         for child in node.children:
             if child.char == char:
-                # We found the char existing in the child.
                 char_not_found = False
-                # Assign node as the child containing the char and break
                 node = child
                 break
-        # Return False anyway when we did not find a char.
         if char_not_found:
             return 0
-    # Well, we are here means we have found the prefix. Return true to indicate that
-    # And also the counter of the last node. This indicates how many ints have this
-    # prefix
     return prefix
 
 if __name__ == "__main__":
     root = TrieNode('*')
     numbers = []
     print ('starting')
-    with open("/home/mahdi/Desktop/Projects/programming_class_cheats/tmu_ctf_too_many_challenge/numbers.txt") as f:
+    with open("numbers.txt") as f:
         content = f.readlines()
     for n in content:
         numbers.append(int(n.strip()))
@@ -85,10 +64,14 @@ def func(x):
     for i in numbers:
         j = x - i # we are looking for j where j+i == x
         a=find_prefix(root, j)
-        if a!=0 and a!=i and a > i:
-            ans.add((a,i))
-        if a!=0 and a!=i and a < i:
-            ans.add((i,a))
+        if a!=0:
+            if a == i:
+                continue
+            elif a > i:
+                ans.add((a,i))
+            else:
+                ans.add((i,a))
+    print(len(ans))
     return len(ans)
 
 
